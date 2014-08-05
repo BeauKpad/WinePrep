eab2667bd10b9f95b2b4ef7a3254f74d4aab22fb
S. LeBeau Kpadenou <LeBeau@LeBeauK.com>
2014-08-05 02:39:41 -0400

    Initial

---
+++ src/CardActivity.java
@@ -0,0 +1,15 @@
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class CardActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.flshcard);
+	}
+
+}
